440bbc0974b41d45ae59515e334c6f057c1bb20c
David Steinberg <david.steinberg17@gmail.com>
2022-07-14 15:26:47 +0200

    emoty

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,24 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        }
+        else if(query.contains(",")){
+            String[] numbers = query.split(", ");
+            int maxNumb = 0;
+            for(int i = 0; i < numbers.length;i++){
+                if(Integer.parseInt(numbers[i]) >= maxNumb){
+                    maxNumb = Integer.parseInt(numbers[i]);
+                }
+            }
+            return String.valueOf(maxNumb);
+        }
+        else if(query.contains("plus")){
+            String[] numbers = query.split(" plus ");
+            int result = 0;
+            result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            return String.valueOf(result);
+        }
+        else{
             return "";
         }
     }
